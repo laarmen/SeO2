6dc975b2c29cb52832bff0f97b3fe133be3dd68a
Simon Chopin <chopin.simon@gmail.com>
2017-10-11 13:20:59 +0200

    Use the already developped nom Lua parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+extern crate nom_lua53;
+
+use std::io::{self, Read};
+use nom_lua53::{parse_all, ParseResult, Statement};
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = Vec::new();
+    io::stdin().read_to_end(&mut input).expect("Couldn't read from stdin");
+
+    match parse_all(&*input) {
+        ParseResult::Done(blk) => {
+            for stmt in blk.stmts {
+                println!("{:?}", stmt);
+                match stmt {
+                    Statement::LVarAssign(ass) => {
+                        println!("Assigning {:?} to {:?}", ass.vals, ass.vars);
+                    }
+                    Statement::Assignment(ass) => {
+                        println!("Assigning {:?} to {:?}", ass.vals, ass.vars);
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        ParseResult::Error(rest, ss) => {
+            println!("Error. statements == {:#?}", ss);
+            println!("rest == '{}'", String::from_utf8_lossy(rest));
+        }
+    }
 }
