63530e558356dbb043e77f475ddd22bb7d17a941
Simon Chopin <chopin.simon@gmail.com>
2017-10-11 16:11:22 +0200

    New enum to represent a runtime value

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ extern crate nom_lua53;
 use std::io::{self, Read};
 use nom_lua53::{parse_all, ParseResult, Statement};
 
+#[derive(Debug)]
+enum LuaValue {
+    Integer(isize),
+    Float(f64),
+    Boolean(bool),
+    Str(String),
+}
+
 fn main() {
     let mut input = Vec::new();
     io::stdin().read_to_end(&mut input).expect("Couldn't read from stdin");
