94dbd398aab0a00209e8428b7b21048e5ae9e18e
Simon Chopin <chopin.simon@gmail.com>
2018-03-26 22:34:15 +0200

    Move the statement execution to the flow control file
    It makes much more sense to have it there, to be honest...

--- src/control_flow.rs
+++ src/control_flow.rs
@@ -1,47 +1,96 @@
 use nom_lua53;
+use nom_lua53::Statement;
 use nom_lua53::stat_expr_types::Block;
 use types::LuaState;
 use expression;
 use std;
-use super::{Result, parse_statement};
+use super::{Result, var_to_string};
 use super::types::LuaValue;
 
-pub enum FlowDisruption {
+pub enum FlowControl {
     None,
     Return(Vec<LuaValue>),
     Break,
 }
 
-pub fn parse_block(block: &Block, ctx: &mut LuaState) -> Result<FlowDisruption> {
-        for stmt in block.stmts.iter() {
-            parse_statement(&stmt, ctx)?;
-        };
-        if let Some(ref expressions) = block.ret_stmt {
-            let mut results = std::vec::Vec::with_capacity(expressions.len());
-            for exp in expressions {
-                results.push(expression::eval_expr(exp, ctx)?)
-            }
-            Ok(FlowDisruption::Return(results))
-        } else {
-            Ok(FlowDisruption::None)
+pub fn exec_statement(stmt: &Statement, ctx: &mut LuaState) -> Result<FlowControl> {
+    match stmt {
+        &Statement::LVarAssign(ref ass) => {
+            let values = ass.vals
+                .as_ref()
+                .expect("There should be some values. Why isn't there any value?!");
+            for (var, val) in ass.vars.iter().zip(values.iter()) {
+                let computed_value = expression::eval_expr(val, &ctx);
+                let local_scope = ctx.get_mutable_local_scope().unwrap();
+                local_scope.set_string(var_to_string(var), &computed_value.unwrap());
+            };
+            Ok(FlowControl::None)
         }
+        &Statement::Assignment(ref ass) => {
+            let mut values = Vec::with_capacity(ass.vals.len());
+            for exp in ass.vals.iter() {
+                values.push(expression::eval_expr(&exp, &ctx)?);
+            }
+
+            for (prefexp, val) in ass.vars.iter().zip(values.drain(..)) {
+                let assignment = expression::prefixexp::resolve_prefix_expr(
+                    &prefexp.prefix,
+                    &prefexp.suffix_chain,
+                    ctx,
+                )?;
+                assignment.environment.set(&assignment.index, &val)?;
+            }
+            println!("Assigning {:?} to {:?}", ass.vals, ass.vars);
+            Ok(FlowControl::None)
+        }
+        &Statement::Semicolon => {
+            Ok(FlowControl::None)
+        }
+        &Statement::Ite(ref ite) => {
+            exec_if_then_else(ite, ctx)
+        }
+        _ => {Ok(FlowControl::None)}
+    }
 }
-pub fn exec_if_then_else(ite: &nom_lua53::IfThenElse, ctx: &mut LuaState) -> Result<FlowDisruption> {
+
+
+pub fn exec_block(block: &Block, ctx: &mut LuaState) -> Result<FlowControl> {
+    for stmt in block.stmts.iter() {
+        match exec_statement(&stmt, ctx)? {
+            FlowControl::Break => return Ok(FlowControl::Break),
+            FlowControl::Return(val) => return Ok(FlowControl::Return(val)),
+            FlowControl::None => {}
+        }
+    };
+    let ret = if let Some(ref expressions) = block.ret_stmt {
+        let mut results = std::vec::Vec::with_capacity(expressions.len());
+        for exp in expressions {
+            results.push(expression::eval_expr(exp, ctx)?)
+        }
+        Ok(FlowControl::Return(results))
+    } else {
+        Ok(FlowControl::None)
+    };
+
+    return ret
+        }
+
+pub fn exec_if_then_else(ite: &nom_lua53::IfThenElse, ctx: &mut LuaState) -> Result<FlowControl> {
     if expression::boolean_coercion(&expression::eval_expr(&ite.cond, ctx)?) {
-        parse_block(&ite.then_blk, ctx)
+        exec_block(&ite.then_blk, ctx)
     } else {
         match ite.elseifs.iter().fold(None,
          |current, &(ref exp, ref block)|
           if current.is_none() {
               match expression::eval_expr(&exp, ctx) {
-                  Ok(_) => Some(parse_block(&block, ctx)),
+                  Ok(_) => Some(exec_block(&block, ctx)),
                   Err(err) => Some(Err(err))
               }
           } else { current }) {
               Some(val) => val,
               None => match &ite.else_blk {
-                  &Some(ref blk) => parse_block(blk, ctx),
-                  &None => Ok(FlowDisruption::None)
+                  &Some(ref blk) => exec_block(blk, ctx),
+                  &None => Ok(FlowControl::None)
               }
           }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 extern crate nom_lua53;
 
-use nom_lua53::{parse_all, ParseResult, Statement};
+use nom_lua53::{parse_all, ParseResult};
 use nom_lua53::name::VarName;
 
 mod expression;
@@ -22,53 +22,11 @@ pub fn var_to_string(var: &VarName) -> String {
     String::from_utf8_lossy(var.0).to_string()
 }
 
-pub fn parse_statement(stmt: &Statement, ctx: &mut types::LuaState) -> Result<control_flow::FlowDisruption> {
-    match stmt {
-        &Statement::LVarAssign(ref ass) => {
-            let values = ass.vals
-                .as_ref()
-                .expect("There should be some values. Why isn't there any value?!");
-            for (var, val) in ass.vars.iter().zip(values.iter()) {
-                let computed_value = expression::eval_expr(val, &ctx);
-                let local_scope = ctx.get_mutable_local_scope().unwrap();
-                local_scope.set_string(var_to_string(var), &computed_value.unwrap());
-            }
-        }
-        &Statement::Assignment(ref ass) => {
-            let mut values = Vec::with_capacity(ass.vals.len());
-            for exp in ass.vals.iter() {
-                values.push(expression::eval_expr(&exp, &ctx)?);
-            }
-
-            for (prefexp, val) in ass.vars.iter().zip(values.drain(..)) {
-                let assignment = expression::prefixexp::resolve_prefix_expr(
-                    &prefexp.prefix,
-                    &prefexp.suffix_chain,
-                    ctx,
-                )?;
-                assignment.environment.set(&assignment.index, &val)?;
-            }
-            println!("Assigning {:?} to {:?}", ass.vals, ass.vars);
-        }
-        &Statement::Semicolon => {
-            println!("Semicolon...");
-        }
-        &Statement::Ite(ref ite) => {
-            return control_flow::exec_if_then_else(ite, ctx);
-        }
-        _ => {}
-    }
-    return Ok(control_flow::FlowDisruption::None);
-}
-
 pub fn eval_file(input: &[u8]) -> Result<()> {
     let mut ctx = types::LuaState::new();
     match parse_all(input) {
         ParseResult::Done(blk) => {
-            for stmt in blk.stmts {
-                println!("{:?}", stmt);
-                parse_statement(&stmt, &mut ctx)?;
-            }
+            let _ = control_flow::exec_block(&blk, &mut ctx);
             println!("{:?}", ctx.get_local_scope());
             println!("{:?}", ctx);
         }
